efe5f794269f512894e550d0218d316789e9ef90
Bryan <mcgahan72@outlook.com>
2022-05-05 20:36:24 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,238 @@
+<script>
+</script>
+
+<template>
+  <div id="app" class="">
+    <div id="main" class="md:h-screen">
+      <header class="px-5 py-5 flex justify-center mb-4">
+        <div class="flex items-center space-x-1">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              width="48"
+              fill="#008f2b"
+              class=""
+            >
+              <path
+                d="M4 40V37H16Q15 32.45 11.625 29.025Q8.25 25.6 4 24.5Q5 24.25 5.975 24.125Q6.95 24 8 24Q14.7 24 19.35 28.65Q24 33.3 24 40ZM27 40Q27 37.9 26.6 36.325Q26.2 34.75 25.6 33.2Q27.6 28.8 31.6 26.4Q35.6 24 40 24Q41.05 24 42.025 24.125Q43 24.25 44 24.5Q39.75 25.6 36.3 29.075Q32.85 32.55 32 37H44V40ZM24.1 30.35Q23.65 26.55 24.75 23.25Q25.85 19.95 27.975 17.5Q30.1 15.05 33.1 13.6Q36.1 12.15 39.45 12.05Q36.65 13.8 34.175 16.825Q31.7 19.85 31.05 23.3Q28.85 24.35 27.05 26.225Q25.25 28.1 24.1 30.35ZM21.05 26.55Q20.25 25.8 19.3 25.05Q18.35 24.3 17.7 23.9Q17.85 23.3 17.925 22.575Q18 21.85 18 21.4Q18 17.6 16.8 14.2Q15.6 10.8 13.4 8Q16.9 9.4 19.6 12.425Q22.3 15.45 23.4 19.35Q22.5 20.95 21.875 22.825Q21.25 24.7 21.05 26.55Z"
+              />
+            </svg>
+          </div>
+          <div>
+            <span class="text-4xl font-extrabold text-slate-50"
+              >Canada Thistle</span
+            >
+          </div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              width="48"
+              fill="#008f2b"
+            >
+              <path
+                d="M4 40V37H16Q15 32.45 11.625 29.025Q8.25 25.6 4 24.5Q5 24.25 5.975 24.125Q6.95 24 8 24Q14.7 24 19.35 28.65Q24 33.3 24 40ZM27 40Q27 37.9 26.6 36.325Q26.2 34.75 25.6 33.2Q27.6 28.8 31.6 26.4Q35.6 24 40 24Q41.05 24 42.025 24.125Q43 24.25 44 24.5Q39.75 25.6 36.3 29.075Q32.85 32.55 32 37H44V40ZM24.1 30.35Q23.65 26.55 24.75 23.25Q25.85 19.95 27.975 17.5Q30.1 15.05 33.1 13.6Q36.1 12.15 39.45 12.05Q36.65 13.8 34.175 16.825Q31.7 19.85 31.05 23.3Q28.85 24.35 27.05 26.225Q25.25 28.1 24.1 30.35ZM21.05 26.55Q20.25 25.8 19.3 25.05Q18.35 24.3 17.7 23.9Q17.85 23.3 17.925 22.575Q18 21.85 18 21.4Q18 17.6 16.8 14.2Q15.6 10.8 13.4 8Q16.9 9.4 19.6 12.425Q22.3 15.45 23.4 19.35Q22.5 20.95 21.875 22.825Q21.25 24.7 21.05 26.55Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </header>
+      <div
+        class="
+          bg-slate-500
+          rounded-xl
+          container
+          mx-auto
+          grid grid-cols-1
+          gap-y-2
+          md:grid-cols-3 md:gap-x-2
+          p-5
+          h-full
+        "
+      >
+        <div class="card bg-zinc-300 rounded-xl px-4 py-8 overflow-auto">
+          <div class="card-header">
+            <p class="text-xl font-bold border-b mb-4">
+              United States Views on Agriculture and Natural Ecosystems
+            </p>
+          </div>
+          <div class="card-body">
+            <img
+              src="./assets/usnature.jpg"
+              alt="Nature"
+              class="w-52 rounded-xl shadow-md float-right"
+            />
+            <p class="text-md font-light">
+              How important are natural ecosystems to mankind? Well, if we ask
+              this question in the United States we would get a wide range of
+              different answers. To some people natural ecosystems and nature in
+              general is the most important aspect of our world. To other people
+              in the country, nature is not important at all, if we look at the
+              article “How Much Nature Should America Keep?”, we are able to see
+              both sides of the story. Lee-Ashley claims that “Every 30 seconds,
+              a football field worth of America’s natural areas disappears to
+              roads, houses, pipelines, and other development.” (Lee-Ashley).
+              The sad reality of America’s views on nature is that we seem focus
+              more on development, than the preservation of nature. Now while
+              this is a sad reality for most of Americans’ there are still a
+              portion of America that understands the necessity of preserving
+              our natural ecosystems. One of the most important figures in
+              American Conservation was Theodore Roosevelt, from a young age he
+              was fascinated by nature and the wildlife in it. As president
+              Roosevelt used his power to establish 150 national forests, 51
+              federal bird reserves, four national game preserves, five
+              nationals parks and 18 national monuments on over 230 million
+              acres of public land. Roosevelt did many great things for
+              conservation while he was president, however he also inspired
+              following generations to understand the beauty and necessity of
+              nature. While the United States is a country of beautiful nature,
+              we are also, a large player in agriculture. In the United States
+              about 40 percent of the land is used for agriculture in one form
+              or another. Most of this farmland is used by small farms almost 91
+              percent, and the remaining 9 percent is occupied by large farms.
+              These numbers help us realize just how important agriculture is in
+              the United States. While development is overtaking too much land
+              of natural ecosystems and agriculture, we are still able to see
+              how important both are in the scope of beauty and necessity.
+              Lee-Ashley expresses the opinion that it is necessary for the
+              United States to retain 30 percent of all land for natural
+              ecosystems and agricultural purposes
+            </p>
+          </div>
+        </div>
+        <div class="card bg-zinc-300 rounded-xl px-4 py-8 overflow-auto">
+          <div class="card-header">
+            <p class="text-xl font-bold border-b mb-4">
+              United States Views on Agriculture and Natural Ecosystems
+            </p>
+          </div>
+          <div class="card-body">
+            <img
+              src="./assets/canada-thistle.jpg"
+              alt="Nature"
+              class="w-52 rounded-xl shadow-md float-right"
+            />
+            <p class="text-md font-light">
+              Why are some plants regarded as "good" while some are regarded as
+              "bad"? What makes a plant good or bad? Growing up around farming
+              and ranching in west central Nebraska, I was introduced to this
+              idea of a good or a bad plant at a young age. One of the worst
+              plants that I remember is Canada Thistle. So naturally I asked the
+              question of why it was bad, well when I was younger no one gave me
+              a straight answer, but as I dug into why it was considered bad I
+              found lots of material on why it is bad. The general sentiment
+              surrounding Canada Thistle is that it is a very bad weed, being
+              labeled as Noxious in at least 35 other states (Wilson). Why is
+              Canada Thistle noxious? Canada thistle invades natural areas such
+              as prairies, savannas, glades and dunes if some degree of
+              disturbance already exists. Once established Canada Thistle
+              spreads quickly, replacing native plants. It has high dispersal
+              rates and can remain viable in the soil for up to 20 years
+              (Carelton). Reine describes Canada Thistle as one of Canada's
+              nastier pests. I really tried to find any sort of sentiment around
+              Canada Thistle that was even slightly positive. In the search for
+              positivity surrounding this plant I did come across the edibility
+              of thistle. Elpel talks about the tastiness of a young Canada
+              Thistle's leaves, he talks about the nutritional value that Canada
+              Thistle has, however outside of this and very few medicinal
+              applications, Canada Thistle is still regarded as a very bad weed
+              that needs to be controlled (Elpel's).
+            </p>
+          </div>
+        </div>
+        <div class="card bg-zinc-300 rounded-xl px-4 py-8 overflow-auto">
+          <div class="card-header">
+            <p class="text-xl font-bold border-b mb-4">
+              United States Views on Agriculture and Natural Ecosystems
+            </p>
+          </div>
+          <div class="card-body">
+            <img
+              src="./assets/land.jpg"
+              alt="Nature"
+              class="w-52 rounded-xl shadow-md float-right"
+            />
+            <p class="text-md font-light">
+              Knowing the views around nature and agriculture, and the views
+              surrounding Canada Thistle allows us to make some connections
+              between the United States viewpoints and our ability to address
+              the invasive species. When thinking of the social aspects of our
+              "wicked problem" the first thing that comes to mind is how does
+              the problem directly impact society, however in this case Canada
+              Thistle doesn't necessarily directly impact society. The plant
+              does not directly impact human health, as seen in the views on it,
+              it is believed to be nutritional if consumed. Where the impact
+              does take place however is, in the impact the thistle has on
+              natural ecosystems and agriculture. Thistle has the capability to
+              spared very aggressively and push out native species, which can be
+              and often is very detrimental to natural ecosystems. When it comes
+              to agriculture Canada Thistle is also very damaging, it tends to
+              take over plowed lands (Elpel). Canada Thistle effects more than
+              just land however it is harmful to livestock and wildlife. How
+              does knowing the negative impacts of Canada Thistle help us to
+              understanding the interactions between the viewpoints in America
+              and addressing the problem of Canada Thistle? We saw earlier that
+              even though a lot of land is wiped out for development purposes,
+              there is still a lot people that hold the view that nature and
+              agriculture is vitally important. When looking into nature and
+              natural ecosystems, we are interested mostly in the beauty of it.
+              So in the 30 some states where Canada Thistle is identified as a
+              noxious weed we are trying to maintain the natural beauty by
+              managing the species. So what is involved in the management of the
+              species, well to simplify it, a lot of time and money. What I am
+              most concerned with is the time, as a teenager I would spend many
+              of my weekends helping a family friend cut out thistle plants
+              which never solved the problem it just kept it under control. This
+              is what makes the Canada Thistle a wicked problem, no matter what
+              solution you try to implement, you are never really solving the
+              problem you are just controlling it for a period. I don't know if
+              the last example is a great to show the social aspects of Canada
+              Thistle. Canada Thistle in the United States doesn't have the
+              social implications of sending our society into poverty or civil
+              unrest but the implications that it does have is costing many
+              farmers and ranchers’ hours and hours of time trying to control
+              it. Ranchers must remove it not just because in most states it is
+              a noxious species but because cattle will not eat it, ultimately
+              causing ranchers precious pastureland. Farmers must remove it
+              because if it over grows a plowed field the crop being planted
+              will most likely not grow. The ultimate interaction between Canada
+              Thistle and our viewpoints on natural ecosystems and agriculture
+              is that Canada Thistle is a threat to our most valuable resource
+              which is our nature and agriculture. Since Canada Thistle is a
+              threat we must do everything in our power to control it. An
+              important question to ask is how we can control this wicked
+              problem, without infringing on our viewpoints. We must take
+              precautions when attempting to control the problem without making
+              new problems, we don't want to introduce a new species to kill of
+              thistle and have the new species, we don't want to try to kill it
+              with a chemical that just makes everything in a pasture inedible.
+              Canada Thistle threatens our viewpoints and we must control it
+              without causing more wicked problems.
+            </p>
+          </div>
+        </div>
+      </div>
+      <footer class="text-center pb-5 mt-5">
+        <p class="text-sm font-light text-slate-600 mb-2">
+          Created by Bryan McGahan for ARGO 107 Final
+        </p>
+        <a
+          href="/References.pdf#toolbar=0"
+          class="text-sm text-blue-700 bg-slate-200 px-2 py-2 rounded-xl"
+        >
+          <span
+            >Resources
+            <span
+              class="w-3 h-3 rounded-full bg-sky-600 animate-ping absolute"
+            ></span>
+            <span class="w-3 h-3 rounded-full bg-sky-600 absolute"></span
+          ></span>
+        </a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
